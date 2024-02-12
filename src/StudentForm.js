a07d8cd96d3d020a85668a3137be1138b2d23427
kelly_maass <kmaass77627@gmail.com>
2024-02-11 19:09:25 -0600

    done

--- src/StudentForm.js
+++ src/StudentForm.js
@@ -9,7 +9,7 @@ function StudentForm() {
         points: 0
        })
 
-       function handleChange(e) {
+       function handleFirstChange(e) {
         console.log(e.target.value)
         setFormData({
             ...formData, 
@@ -17,20 +17,56 @@ function StudentForm() {
         })
        }
 
+       function handleLastChange(e) {
+        console.log(e.target.value)
+        setFormData({
+            ...formData, 
+            lastName: e.target.value
+        })
+       }
+
+       function handleImageChange(e) {
+        console.log(e.target.value)
+        setFormData({
+            ...formData, 
+            image: e.target.value
+        })
+       }
+
+       function handlePointsChange(e) {
+        console.log(e.target.value)
+        setFormData({
+            ...formData, 
+            points: e.target.value
+        })
+       }
+
+       function handleSubmit(e) {
+        e.preventDefault()
+            console.log(formData)
+            fetch("http://localhost:3006/students", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json"
+                }, 
+                body: JSON.stringify(formData)
+              })
+       }
+
 
 
     return (
         <div id="form">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h3>Add Student</h3>
                 <label>first name</label>
-                <input onChange={handleChange} type="text" name="firstname" value={formData.firstName}></input>
+                <input onChange={handleFirstChange} type="text" name="firstname" value={formData.firstName}></input>
                 <label>last name</label>
-                <input onChange={handleChange} type="text" name="lastname" value={formData.lastName}></input>
+                <input onChange={handleLastChange} type="text" name="lastname" value={formData.lastName}></input>
                 <label>image</label>
-                <input onChange={handleChange} type="text" name="image" value={formData.image}></input><br></br>
+                <input onChange={handleImageChange} type="text" name="image" value={formData.image}></input><br></br>
                 <label>points</label>
-                <input onChange={handleChange} type="text" name="points" value={formData.points}></input><br></br>
+                <input onChange={handlePointsChange} type="text" name="points" value={formData.points}></input><br></br>
                 <button>Submit</button>
             </form>
 
