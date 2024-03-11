6a5b60a9ccf55732cdeeb6b59f06f863f8f49e83
kelly_maass <kmaass77627@gmail.com>
2024-03-11 15:07:45 -0500

    initial commit

--- src/Parent.js
+++ src/Parent.js
@@ -5,7 +5,7 @@ function Parent({parent}) {
 
     return(
         <div id="parent">
-           <h3>Parent Name: {parent.name} </h3>
+           <h3>Parent Name: {parent.name} </h3><br></br>
            <h4> Contact: {parent.phone}</h4>
         </div>
     )
