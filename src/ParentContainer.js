f79e8b09287267a2dff556d1691ae81cd7fb7f94
kelly_maass <kmaass77627@gmail.com>
2024-11-18 18:39:07 -0600

    done

--- src/ParentContainer.js
+++ src/ParentContainer.js
@@ -5,7 +5,7 @@ function ParentContainer({parents, students}) {
     // console.log(students)
 
     return(
-        <div className="item4">
+        <div id="parents">
             <h2 id="h2">Parents</h2>
             {parents.map((parent) => <Parent key={parent.id} parent={parent} students={students}></Parent>)}
         </div>
